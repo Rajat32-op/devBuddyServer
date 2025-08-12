d15b5a63fa88cccb08c8006e469264cda0107e93
Rajat <rajathegde2006@gmail.com>
2025-08-12 11:25:49 +0530

    chat delete fixxed

--- services/handleMessage.js
+++ services/handleMessage.js
@@ -1,20 +1,20 @@
 const Message = require('../models/Message');
 const User = require('../models/User')
-const {storage}=require('./cloudinary')
-const upload=require('multer')({storage});
+const { storage ,cloudinary} = require('./cloudinary')
+const upload = require('multer')({ storage });
 
-const uploadChatImage=async(req,res,next)=>{
+const uploadChatImage = async (req, res, next) => {
     console.log('hello');
-    upload.array('images',10)(req,res,(err)=>{
-    if(err){
-      return res.status(400).json(err)
-    }
-    next();
-  });
+    upload.array('images', 10)(req, res, (err) => {
+        if (err) {
+            return res.status(400).json(err)
+        }
+        next();
+    });
 }
 
 async function getchats(req, res) {
-    try{
+    try {
 
         const recentChats = await Message.aggregate([
             {
@@ -22,7 +22,8 @@ async function getchats(req, res) {
                     $or: [
                         { senderId: req.user._id },
                         { receiverId: req.user._id }
-                    ]
+                    ],
+                    deletedBy: { $ne: req.user._id }
                 }
             },
             {
@@ -56,36 +57,64 @@ async function getchats(req, res) {
                 $sort: { lastMessageTime: -1 }
             }
         ]);
-        
-        
+
+
         const users = await User.find({ _id: { $in: recentChats.map(c => c._id) } }).select("_id name username profilePicture")
         const userMap = {};
         users.forEach(user => userMap[user._id.toString()] = user);
         // Merge
-        const sortedChats = recentChats.map(c => ({...userMap[c._id.toString()]._doc,lastMessageTime:c.lastMessageTime}));
-        
+        const sortedChats = recentChats.map(c => ({ ...userMap[c._id.toString()]._doc, lastMessageTime: c.lastMessageTime }));
+
         return res.status(200).json(sortedChats)
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.status(500)
     }
 }
 
 async function deleteChat(req, res) {
-    const roomId = req.body.roomId;
-    try {
+    const { roomId } = req.body;
 
-        await Message.updateMany({ roomId: roomId },
-            { $addToSet: { deletedBy: req.user._id } })
-        return res.status(200)
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(500)
+    try {
+        // Step 1: Mark messages as deleted by current user
+        await Message.updateMany(
+            { roomId: roomId },
+            { $addToSet: { deletedBy: req.user._id } }
+        );
+
+        // Step 2: Find messages to permanently delete (deletedBy size = 2)
+        const toDelete = await Message.find({
+            roomId: roomId,
+            $expr: { $eq: [{ $size: "$deletedBy" }, 2] }
+        });
+
+        if (toDelete.length > 0) {
+            // Step 3: Collect all Cloudinary IDs
+            const allImageIds = toDelete
+                .flatMap(msg => msg.imageIds || []);
+
+            // Step 4: Delete images from Cloudinary
+            for (const id of allImageIds) {
+                try {
+                    await cloudinary.uploader.destroy(id);
+                } catch (err) {
+                    console.error(`Failed to delete Cloudinary image: ${id}`, err);
+                }
+            }
+
+            // Step 5: Delete messages from DB
+            await Message.deleteMany({ _id: { $in: toDelete.map(m => m._id) } });
+        }
+
+        return res.status(200).json({ message: "successful" });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "error" });
     }
 }
 
 module.exports = {
-    deleteChat,getchats,uploadChatImage
+    deleteChat, getchats, uploadChatImage
 }
